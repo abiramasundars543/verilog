92cb863fa61622a3cea5d1d63f585f4ef6ef5054
abiramasundars543 <abiramasundars543@gmail.com>
2025-03-01 23:55:59 +0530

    Add modified file

--- mux_demo.v
+++ mux_demo.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.02.2025 12:35:38
-// Design Name: 
-// Module Name: mux_demo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 
